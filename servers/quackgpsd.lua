9dfb7e4935a7a3c3219436dd87c57cabf6e61bd7
Tulir Asokan <tulir@maunium.net>
2017-05-03 23:27:47 +0300

    Update quackgpsd to use libquacknet 1.0 functions

--- servers/quackgpsd.lua
+++ servers/quackgpsd.lua
@@ -2,31 +2,27 @@ term.setTextColor(colors.yellow)
 print("QuackGPSd 0.1 started")
 
 while true do
-	local sender, message = quacknet.receive()
-	local data = textutils.unserialize(message)
-	if not data then
+	local msg = quacknet.listen()
+	if not msg.data then
 		term.setTextColor(colors.orange)
 		print("Invalid request from " .. sender)
-		quacknet.send(sender,
-			textutils.serialize({
-				error = "Invalid data format"
-			}))
+		msg.reply({
+			error = "Invalid data format"
+		})
 	else
-		if data.command == "track" then
+		if msg.data.command == "track" then
 			term.setTextColor(colors.cyan)
-			print("Request to track " .. data.player .. " from " .. sender)
-			local x, y, z = libquackgps.track(data.player)
-			quacknet.send(sender,
-				textutils.serialize({x=x, y=y, z=z}))
+			print("Request to track " .. msg.data.player .. " from " .. msg.sender)
+			local x, y, z = libquackgps.track(msg.data.player)
+			msg.reply({x=x, y=y, z=z})
 		elseif data.command == "list" then
-			quacknet.send(sender, textutils.serialize(libquackgps.getNames(true)))
+			msg.reply(libquackgps.getNames(true))
 		else
 			term.setTextColor(colors.orange)
 			print("Unknown command " .. data.command .. " from " .. sender)
-			quacknet.send(sender,
-				textutils.serialize({
-					error = "Unknown command!"
-				}))
+			msg.reply({
+				error = "Unknown command!"
+			})
 		end
 	end
 end
