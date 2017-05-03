a1c4756fa5dfed24fbfcb6bd67670cbbc963f611
Tulir Asokan <tulir@maunium.net>
2017-05-04 01:45:32 +0300

    Add rounding for quacktrack

--- programs/quacktrack.lua
+++ programs/quacktrack.lua
@@ -5,6 +5,11 @@ if table.getn(args) < 1 then
 	return
 end
 
+local function round(num, numDecimalPlaces)
+	local mult = 10^(numDecimalPlaces or 0)
+	return math.floor(num * mult + 0.5) / mult
+end
+
 -- TODO unhardcode GPS master ID
 
 local player = args[1]
@@ -18,7 +23,7 @@ if os.getComputerID() == 39 then
 	else
 		local x, y, z = libquackgps.track(player)
 		term.setTextColor(colors.green)
-		print(player, " is at ", x, ", ", y, ", ", z)
+		print(player, " is at ", round(x, 1), ", ", round(y, 1), ", ", round(z, 1))
 	end
 else
 	local reply = quacknet.request(39, textutils.serialize({
@@ -29,7 +34,7 @@ else
 	if reply.data and reply.data.success then
 		local pos = reply.data
 		term.setTextColor(colors.green)
-		print(player, " is at ", pos.x, ", ", pos.y, ", ", pos.z)
+		print(player, " is at ", round(pos.x, 1), ", ", round(pos.y, 1), ", ", round(pos.z, 1))
 	else
 		term.setTextColor(colors.red)
 		print(reply.error)
