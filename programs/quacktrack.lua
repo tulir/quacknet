d466eb63c762c245cc835e919b68052851b9858e
Tulir Asokan <tulir@maunium.net>
2017-05-03 23:10:19 +0300

    Add local quacktrack support

--- programs/quacktrack.lua
+++ programs/quacktrack.lua
@@ -5,16 +5,33 @@ if table.getn(args) < 1 then
 	return
 end
 
-local reply = quacknet.request(39, textutils.serialize({
-	command = "track",
-	player = args[1]
-}))
+-- TODO unhardcode GPS master ID
 
-if reply.success then
-	pos = reply.data
-	term.setTextColor(colors.green)
-	print(args[1], " is at ", pos.x, ", ", pos.y, ", ", pos.z)
+local player = args[1]
+if os.getComputerID() == 39 then
+	if not libquackgps.isOnline(player) then
+		term.setTextColor(colors.orange)
+		print("Player not online.")
+	elseif not libquackgps.isInWorld(player) then
+		term.setTextColor(colors.orange)
+		print("Player not in this dimension.")
+	else
+		local x, y, z = libquackgps.track(player)
+		term.setTextColor(colors.green)
+		print(player, " is at ", x, ", ", y, ", ", z)
+	end
 else
-	term.setTextColor(colors.red)
-	print(reply.error)
+	local reply = quacknet.request(39, textutils.serialize({
+		command = "track",
+		player = player
+	}))
+
+	if reply.success then
+		local pos = reply.data
+		term.setTextColor(colors.green)
+		print(player, " is at ", pos.x, ", ", pos.y, ", ", pos.z)
+	else
+		term.setTextColor(colors.red)
+		print(reply.error)
+	end
 end
