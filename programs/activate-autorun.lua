211770a03ba252d7dd5b8091bea6292daf26676c
Tulir Asokan <tulir@maunium.net>
2017-05-03 23:20:02 +0300

    Minor fixes

--- programs/activate-autorun.lua
+++ programs/activate-autorun.lua
@@ -1,12 +1,12 @@
-args = { ... }
+local args = { ... }
 if table.getn(args) < 1 then
 	term.setTextColor(colors.red)
 	print("Usage: activate-autorun <file> [args...]")
 	return
 end
 
-filePath = args[1]
-fileName = table.remove(fileName:split("/"))
-file = fs.open("/autorun/" .. fileName, "w")
+local filePath = args[1]
+local fileName = table.remove(filePath:split("/"))
+local file = fs.open("/autorun/" .. fileName, "w")
 file.write("shell.run(\"" .. table.concat(args, " ") .. "\")")
 file.close()
--- servers/quackgpsd.lua
+++ servers/quackgpsd.lua
@@ -1,28 +1,32 @@
+term.setTextColor(colors.yellow)
 print("QuackGPSd 0.1 started")
 
 while true do
-  local sender, message = quacknet.receive()
-  local data = textutils.unserialize(message)
-  if not data then
-    print("Invalid request from " .. sender)
-    quacknet.send(sender,
-      textutils.serialize({
-        error = "Invalid data format"
-      }))
-  else
-    if data.command == "track" then
-      print("Request to track " .. data.player .. " from " .. sender)
-      local x, y, z = libquackgps.track(data.player)
-      quacknet.send(sender,
-        textutils.serialize({x=x, y=y, z=z}))
-    elseif data.command == "list" then
-      quacknet.send(sender, textutils.serialize(libquackgps.getNames(true)))
-    else
-      print("Unknown command " .. data.command .. " from " .. sender)
-      quacknet.send(sender,
-        textutils.serialize({
-          error = "Unknown command!"
-        }))
-    end
-  end
+	local sender, message = quacknet.receive()
+	local data = textutils.unserialize(message)
+	if not data then
+		term.setTextColor(colors.orange)
+		print("Invalid request from " .. sender)
+		quacknet.send(sender,
+			textutils.serialize({
+				error = "Invalid data format"
+			}))
+	else
+		if data.command == "track" then
+			term.setTextColor(colors.cyan)
+			print("Request to track " .. data.player .. " from " .. sender)
+			local x, y, z = libquackgps.track(data.player)
+			quacknet.send(sender,
+				textutils.serialize({x=x, y=y, z=z}))
+		elseif data.command == "list" then
+			quacknet.send(sender, textutils.serialize(libquackgps.getNames(true)))
+		else
+			term.setTextColor(colors.orange)
+			print("Unknown command " .. data.command .. " from " .. sender)
+			quacknet.send(sender,
+				textutils.serialize({
+					error = "Unknown command!"
+				}))
+		end
+	end
 end
