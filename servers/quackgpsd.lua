cacb62d74da6a9de3952c0880440c8ac59568f6b
Tulir Asokan <tulir@maunium.net>
2017-05-04 18:43:47 +0300

    Add track_all command to quackgpsd

--- servers/quackgpsd.lua
+++ servers/quackgpsd.lua
@@ -16,7 +16,11 @@ while true do
 			print("Request to track " .. msg.data.player .. " from " .. msg.sender)
 			local x, y, z = quackgps.track(msg.data.player)
 			msg.reply({success = true, x=x, y=y, z=z})
-		elseif data.command == "list" then
+		elseif msg.data.command == "track_all" then
+			term.setTextColor(colors.cyan)
+			print("Request to track all players in dimension")
+			msg.reply({success = true, players = quackgps.trackAll()}) 
+		elseif msg.data.command == "list" then
 			msg.reply(quackgps.getNames(true))
 		else
 			term.setTextColor(colors.orange)
