54d0be532d210e7baf613688dba11fdde1413aa7
Tulir Asokan <tulir@maunium.net>
2017-05-04 21:44:53 +0300

    Encrypt quackgpsd output

--- servers/quackgpsd.lua
+++ servers/quackgpsd.lua
@@ -15,11 +15,11 @@ while true do
 			term.setTextColor(colors.cyan)
 			print("Request to track " .. msg.data.player .. " from " .. msg.sender)
 			local x, y, z = quackgps.track(msg.data.player)
-			msg.reply({success = true, x=x, y=y, z=z})
+			msg.reply({success = true, x=x, y=y, z=z}, true)
 		elseif msg.data.command == "track_all" then
 			term.setTextColor(colors.cyan)
 			print("Request to track all players in dimension")
-			msg.reply({success = true, players = quackgps.trackAll()}) 
+			msg.reply({success = true, players = quackgps.trackAll()}, true)
 		elseif msg.data.command == "list" then
 			msg.reply(quackgps.getNames(true))
 		else
