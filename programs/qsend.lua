3d0cc6d7bb08039ca323ba872fe606f3ec9474c7
Tulir Asokan <tulir@maunium.net>
2017-06-07 22:18:23 +0300

    Allow using qsend with hostnames

--- programs/qsend.lua
+++ programs/qsend.lua
@@ -3,7 +3,7 @@ if #args < 2 then
 	term.setTextColor(colors.red)
 	print("Usage: qsend <target> <message>")
 end
-target = tonumber(table.remove(args, 1))
+target = table.remove(args, 1)
 local reply = quacknet.request(target, table.concat(args, " "))
 if reply.success then
 	term.setTextColor(colors.green)
