a42c7d7f04e8d24a8211e1619bac53c26e06aa14
Tulir Asokan <tulir@maunium.net>
2017-06-09 10:47:00 +0300

    Improve quackdns command

--- programs/quackdns.lua
+++ programs/quackdns.lua
@@ -1,19 +1,27 @@
 args = { ... }
 if #args < 2 then
 	term.setTextColor(colors.red)
-	print("Usage: quackdns <register/unregister> <domain> [id]")
+	print("Not enough arguments!")
+	print("Usage: quackdns <register/unregister> <domain> ...")
 	return
 end
 command = table.remove(args, 1):lower()
-if command == "register" and #args > 2 then
-	quackdns.addHost(args[2], tonumber(args[3]))
-	term.setTextColor(colors.green)
-	print(args[2], " mapped to ", args[3], " locally")
+if command == "register" then
+	if #args > 2 then
+		quackdns.addHost(args[2], tonumber(args[3]))
+		term.setTextColor(colors.green)
+		print(args[2], " mapped to ", args[3], " locally")
+	else
+		term.setTextColor(colors.red)
+		print("Missing domain target ID!")
+		print("Usage: quackdns register <domain> <id>")
+	end
 elseif command == "unregister" then
 	quackdns.removeHost(args[2])
 	term.setTextColor(colors.green)
 	print(args[2], " mapping removed locally")
 else
 	term.setTextColor(colors.red)
-	print("Usage: quackdns <register/unregister> <domain> [id]")
+	print("Unknown command!")
+	print("Usage: quackdns <register/unregister> <domain> ...")
 end
