733fc2cc362a4a67fdc6bb15985ad5a1b57ac326
Tulir Asokan <tulir@maunium.net>
2017-06-09 10:48:10 +0300

    Fix quackdns command argument handling

--- programs/quackdns.lua
+++ programs/quackdns.lua
@@ -7,19 +7,19 @@ if #args < 2 then
 end
 command = table.remove(args, 1):lower()
 if command == "register" then
-	if #args > 2 then
-		quackdns.addHost(args[2], tonumber(args[3]))
+	if #args > 1 then
+		quackdns.addHost(args[1], tonumber(args[2]))
 		term.setTextColor(colors.green)
-		print(args[2], " mapped to ", args[3], " locally")
+		print(args[1], " mapped to ", args[2], " locally")
 	else
 		term.setTextColor(colors.red)
 		print("Missing domain target ID!")
 		print("Usage: quackdns register <domain> <id>")
 	end
 elseif command == "unregister" then
-	quackdns.removeHost(args[2])
+	quackdns.removeHost(args[1])
 	term.setTextColor(colors.green)
-	print(args[2], " mapping removed locally")
+	print(args[1], " mapping removed locally")
 else
 	term.setTextColor(colors.red)
 	print("Unknown command!")
