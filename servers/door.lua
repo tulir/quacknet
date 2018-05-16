f640066f2017e187a5245f0ebe31a45bb7a2af4b
Tulir Asokan <tulir@maunium.net>
2018-05-16 10:00:46 +0300

    Make door server configurable

--- servers/door.lua
+++ servers/door.lua
@@ -1,19 +1,28 @@
-local prevState = rs.getInput("right")
+args = { ... }
+inputSide = "right"
+outputSide = "left"
+if #args > 0 then
+	inputSide = args[0]
+end
+if #args > 1 then
+	outputSide = args[1]
+end
+local prevState = rs.getInput(inputSide)
 while true do
 	local event, sender, message = os.pullEvent()
-	if event == "redstone" and rs.getInput("right") ~= prevState then
-		if rs.getInput("right") then
-			rs.setOutput("left", not rs.getOutput("left"))
+	if event == "redstone" and rs.getInput(inputSide) ~= prevState then
+		if rs.getInput(inputSide) then
+			rs.setOutput(outputSide, not rs.getOutput(outputSide))
 		end
-		prevState = rs.getInput("right")
+		prevState = rs.getInput(inputSide)
 	elseif event == "rednet_message" then
 		command = quacknet.handleServerReceived(sender, message)
 		if command.success then
 			if command.text == "open" then
-				rs.setOutput("left", false)
+				rs.setOutput(outputSide, false)
 				command.reply("Opening door...")
 			elseif command.text == "close" then
-				rs.setOutput("left", true)
+				rs.setOutput(outputSide, true)
 				command.reply("Closing door...")
 			else
 				command.reply("Unknown command \"" .. command.text .. "\"!")
