f3ff8cc013e91ce21b88efc33c1f4ac484bf75b6
Tulir Asokan <tulir@maunium.net>
2018-05-16 16:48:01 +0300

    Migrate door server to quackserver

--- servers/door.lua
+++ servers/door.lua
@@ -10,30 +10,34 @@ end
 
 local prevState = rs.getInput(inputSide)
 
+local doorserver = quackserver.create("Door Server", "1.0")
+doorserver.registerServiceID("door")
+doorserver.registerDefaultServiceID()
+doorserver.handle("open", function()
+	rs.setOutput(outputSide, true)
+	command.reply("Opening door...")
+end)
+
+doorserver.handle("close", function()
+	rs.setOutput(outputSide, false)
+	command.reply("Closing door...")
+end)
+
+function redstoneListener()
+	while true do
+		os.pullEvent("redstone")
+		if rs.getInput(inputSide) ~= prevState then
+			if rs.getInput(inputSide) then
+				rs.setOutput(outputSide, not rs.getOutput(outputSide))
+			end
+			prevState = rs.getInput(inputSide)
+		end
+	end
+end
+
 print("Door server 1.0 running")
 term.setTextColor(colors.white)
 print("Input:", inputSide)
 print("Output:", outputSide)
 
-while true do
-	local event, sender, message = os.pullEvent()
-	if event == "redstone" and rs.getInput(inputSide) ~= prevState then
-		if rs.getInput(inputSide) then
-			rs.setOutput(outputSide, not rs.getOutput(outputSide))
-		end
-		prevState = rs.getInput(inputSide)
-	elseif event == "rednet_message" then
-		command = quacknet.handleServerReceived(sender, message)
-		if command.success then
-			if command.text == "open" then
-				rs.setOutput(outputSide, true)
-				command.reply("Opening door...")
-			elseif command.text == "close" then
-				rs.setOutput(outputSide, false)
-				command.reply("Closing door...")
-			else
-				command.reply("Unknown command \"" .. command.text .. "\"!")
-			end
-		end
-	end
-end
+parallel.waitForAny(doorserver.start, redstoneListener)
