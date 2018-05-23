d66da846fff763bc729ae4f5d9a657b409cb5f4c
Tulir Asokan <tulir@maunium.net>
2018-05-23 22:06:57 +0300

    Print output in mpf

--- programs/mpf.lua
+++ programs/mpf.lua
@@ -6,37 +6,49 @@ if #args < 1 then
 	return
 end
 
+function print_resp(from, resp)
+	if resp.success then
+		term.setTextColor(colors.green)
+		print(from .. ": " .. resp.text)
+	else
+		term.setTextColor(colors.orange)
+		print(from .. ": " .. resp.error)
+	end
+end
+
 c = args[1]
 if c == "extend" or c == "open" or c == "e" or c == "o" then
 	function open_1()
-		return quacknet.request("mpf-1", {
+		resp = quacknet.request("mpf-1", {
 			service = "mpf",
 			command = "extend",
 		})
+		print_resp("West Controller", resp)
 	end
 	function open_2()
-		return quacknet.request("mpf-2", {
+		resp = quacknet.request("mpf-2", {
 			service = "mpf",
 			command = "extend",
 		})
+		print_resp("East Controller", resp)
 	end
-	resp = parallel.waitForAll(open_1, open_2)
-	print(resp)
+	parallel.waitForAll(open_1, open_2)
 elseif c == "retract" or c == "close" or c == "r" or c == "c" then
 	function close_1()
-		return quacknet.request("mpf-1", {
+		resp = quacknet.request("mpf-1", {
 			service = "mpf",
 			command = "retract",
 		})
+		print_resp("West Controller", resp)
 	end
 	function close_2()
-		return quacknet.request("mpf-2", {
+		resp = quacknet.request("mpf-2", {
 			service = "mpf",
 			command = "retract",
 		})
+		print_resp("East Controller", resp)
 	end
-	resp = parallel.waitForAll(close_1, close_2)
-	print(resp)
+	parallel.waitForAll(close_1, close_2)
 else
 	term.setTextColor(colors.red)
 	print("Usage: mpf <extend/retract>")
